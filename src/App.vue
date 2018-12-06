87fc1f5f4a48208daae93463459ee3376c045df3
sudikm <sudik.maharana@wwindia.com>
2018-12-06 20:09:45 +0530

    app from cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/neosoft.svg">
+    <HelloWorld msg="Building Cohesive Technology Solutions across
+Mobile, Cloud, Web & IoT – for the World’s Leading Enterprises, SMEs & Startups"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
